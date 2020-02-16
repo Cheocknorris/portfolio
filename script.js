86aac5ce20028445ce5c5825bf4cce627f2c15a6
Eliseo Quintanilla <eliseo.quintanillagz@gmail.com>
2020-02-16 14:49:30 +0100

    added animations to landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+(function() {
+    console.log("sanity check");
+    gsap.from('.intro-title-left', { duration: 1, xPercent: 110, delay: .5 })
+    gsap.from('.intro-title-right', { duration: 1, xPercent: -110, delay: .5 })
+    gsap.from('.intro-line', { duration: 1, yPercent: 100, delay: .5 })
+    gsap.from('.intro-sub-left', { duration: .5, yPercent: 100, delay: 1.25 })
+    gsap.from('.intro-sub-right', { duration: .5, yPercent: 100, delay: 1.25 } )
+    gsap.from('.intro-button', { duration: 1.5, opacity: 0, delay: 1.25 } )
+
+}());
