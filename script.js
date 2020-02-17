1bcc8c3fcc3c9105ee487560a791d868eea41cd2
Eliseo Quintanilla <eliseo.quintanillagz@gmail.com>
2020-02-17 16:27:20 +0100

    first scroll animation test

--- script.js
+++ script.js
@@ -7,4 +7,26 @@
     gsap.from('.intro-sub-right', { duration: .5, yPercent: 100, delay: 1.25 } )
     gsap.from('.intro-bottom', { duration: 1.5, opacity: 0, delay: 1.25 } )
 
+    var controller = new ScrollMagic.Controller();
+    var scene = new ScrollMagic.Scene({
+        triggerElement: '.sub'
+    })
+    .setClassToggle('.sub', 'show')
+    .reverse(false)
+    .addTo(controller);
+
+    var scene = new ScrollMagic.Scene({
+        triggerElement: '.sub'
+    })
+    .setClassToggle('.work-line', 'show')
+    .reverse(false)
+    .addTo(controller);
+
+    var scene = new ScrollMagic.Scene({
+        triggerElement: '.project-container'
+    })
+    .setClassToggle('.project-container', 'show')
+    .reverse(false)
+    .addTo(controller);
+
 }());
