d283a1efa725d2636da8a5d866ad14ad81ed13de
Eliseo Quintanilla <eliseo.quintanillagz@gmail.com>
2020-02-19 16:40:26 +0100

    changed the size of the gifs. have 3 out of six project sections complete

--- script-social.js
+++ script-social.js
@@ -3,4 +3,10 @@
     gsap.from('.social-sub', { duration: .5, yPercent: 105, delay: .5 })
     gsap.from('.social-line', { duration: .5, xPercent: -100, delay: .8 })
 
+    gsap.from('.image-sub', { duration: .5, yPercent: 105, delay: .5 })
+    gsap.from('.image-line', { duration: .5, xPercent: -100, delay: .8 })
+
+    gsap.from('.petition-sub', { duration: .5, yPercent: 105, delay: .5 })
+    gsap.from('.petition-line', { duration: .5, xPercent: -100, delay: .8 })
+
 }());
