b8d89d2cc2b0536c53b1774bb32d5866c9a935d2
Eliseo Quintanilla <eliseo.quintanillagz@gmail.com>
2020-07-27 00:06:29 +0200

    start changing classes naming to BEM

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,12 @@
 (function() {
 
 // INTRO ANIMATIONS /////////////////////////////////////////////////////////
-    gsap.from('.intro-title-left', { duration: 1, xPercent: 110, delay: .5 })
-    gsap.from('.intro-title-right', { duration: 1, xPercent: -110, delay: .5 })
-    gsap.from('.intro-line', { duration: 1, yPercent: 100, delay: .5 })
-    gsap.from('.intro-sub-left', { duration: .5, yPercent: 100, delay: 1.25 })
-    gsap.from('.intro-sub-right', { duration: .5, yPercent: 100, delay: 1.25 } )
-    gsap.from('.intro-bottom', { duration: .8, opacity: 0, delay: 1.25 } )
+    gsap.from('.intro__title-left', { duration: 1, xPercent: 110, delay: .5 })
+    gsap.from('.intro__title-right', { duration: 1, xPercent: -110, delay: .5 })
+    gsap.from('.intro__line', { duration: 1, yPercent: 100, delay: .5 })
+    gsap.from('.intro__sub-left', { duration: .5, yPercent: 100, delay: 1.25 })
+    gsap.from('.intro__sub-right', { duration: .5, yPercent: 100, delay: 1.25 } )
+    gsap.from('.intro__bottom', { duration: .8, opacity: 0, delay: 1.25 } )
 /////////////////////////////////////////////////////////////////////////
 
 // CALCULATE TOP OF ELEMENTS FOR SCROLLING ANIMATIONS /////////////////////////////////////////////
@@ -18,9 +18,9 @@
 /////////////////////////////////////////////////////////////////////////////////////
 
 // ABOUT SECTION SCROLLING ANIMATIONS///////////////////////////////////////////////
-    var aboutFirst = document.querySelector('.about-text-one');
+    var aboutFirst = document.querySelector('.about__text-one');
     var aboutFirstOffset = offset(aboutFirst);
-    var aboutLast = document.querySelector('.about-text-seven');
+    var aboutLast = document.querySelector('.about__text-seven');
     var aboutLastOffset = offset(aboutLast);
     // console.log('aboutFirstOffset', aboutFirstOffset.top);
     // console.log('aboutLastOffset', aboutLastOffset.top);
@@ -30,71 +30,70 @@
     var aboutOneAnimation = new ScrollMagic.Scene({
         offset: 300
     })
-    .setClassToggle('.about-text-one', 'up')
+    .setClassToggle('.about__text-one', 'up')
     .reverse(false)
     .addTo(controller);
 
     var aboutTwoAnimation = new ScrollMagic.Scene({
         offset: 350
     })
-    .setClassToggle('.about-text-two', 'up')
+    .setClassToggle('.about__text-two', 'up')
     .reverse(false)
     .addTo(controller);
 
     var aboutThreeAnimation = new ScrollMagic.Scene({
         offset: 400
     })
-    .setClassToggle('.about-text-three', 'up')
+    .setClassToggle('.about__text-three', 'up')
     .reverse(false)
     .addTo(controller);
 
     var aboutFourAnimation = new ScrollMagic.Scene({
         offset: 450
     })
-    .setClassToggle('.about-text-four', 'up')
+    .setClassToggle('.about__text-four', 'up')
     .reverse(false)
     .addTo(controller);
 
     var aboutFiveAnimation = new ScrollMagic.Scene({
         offset: 500
     })
-    .setClassToggle('.about-text-five', 'up')
+    .setClassToggle('.about__text-five', 'up')
     .reverse(false)
     .addTo(controller);
 
     var aboutSixAnimation = new ScrollMagic.Scene({
         offset: 550
     })
-    .setClassToggle('.about-text-six', 'up')
+    .setClassToggle('.about__text-six', 'up')
     .reverse(false)
     .addTo(controller);
 
     var aboutSevenAnimation = new ScrollMagic.Scene({
         offset: 600
     })
-    .setClassToggle('.about-text-seven', 'up')
+    .setClassToggle('.about__text-seven', 'up')
     .reverse(false)
     .addTo(controller);
 
 
 /// WORK TITLE SCROLLING ANIMATION ///////////////////////////////////////////////////////
 
-    var workTitle = document.querySelector('.work-title');
-    var workTitleOffset = offset(workTitle);
-    console.log( 'workTitleOffset:', workTitleOffset.top);
+    var work = document.querySelector('.work');
+    var workOffset = offset(work);
+    console.log( 'workOffset:', workOffset.top);
 
     var workTitleAnimation = new ScrollMagic.Scene({
-        // triggerElement: '.sub'
         offset: 685
     })
-    .setClassToggle('.sub', 'up')
+    .setClassToggle('.work__title', 'up')
     .reverse(false)
     .addTo(controller);
 
     var workUnderlineAnimation = new ScrollMagic.Scene({
         offset: 750
     })
-    .setClassToggle('.work-line', 'right')
+    .setClassToggle('.work__title-underline', 'right')
     .reverse(false)
     .addTo(controller);
 ///////////////////////////////////////////////////////////////////////////////
